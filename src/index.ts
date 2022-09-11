993f54af71cb320f0aceaae7c7831cdfa1716ef6
TheWisePigeon <zozozozeph@gmail.com>
2022-09-11 22:22:57 +0000

    fix: fixed npm dev script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Bruh ok')
+import express, { Response, Request } from "express";
+
+const app = express()
+
+app.get('/', (req: Request, res: Response)=>{
+
+    return res.send('Hello bruh ok')
+})
+
+app.listen(3000, ()=>{
+    console.log('App listening on port 3000');
+    
+})
