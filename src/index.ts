b14551e6586394c250de7e94480ce73c499b8a95
TheWisePigeon <zozozozeph@gmail.com>
2022-09-30 12:42:51 +0000

    style: indented

--- src/index.ts
+++ src/index.ts
@@ -10,47 +10,47 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-app.post('/register', async (req: Request, res: Response)=>{
+app.post('/register', async (req: Request, res: Response) => {
     let { username, password } = req.body
     password = await hashPassword(password)
-    const newUser = new User({username, password})
-    newUser.save((err, result)=>{
+    const newUser = new User({ username, password })
+    newUser.save((err, result) => {
         if (err) {
             return res.status(500).send({
-                "message":`Something went wrong :( \n ${err.message}`
+                "message": `Something went wrong :( \n ${err.message}`
             })
         }
         console.log(`User registered ${result.username}`)
         return res.status(200).send({
-            "message":"Registration completed :)",
-            "data":result
+            "message": "Registration completed :)",
+            "data": result
         })
     })
 })
 
-app.post('/login', async (req: Request, res: Response)=>{
+app.post('/login', async (req: Request, res: Response) => {
     let { username, password } = req.body
     const userAccount = await User.findOne({
-        username:username
+        username: username
     })
-    if (!userAccount){
+    if (!userAccount) {
         return res.status(404).send({
-            "message":`User ${username} not found :(`
+            "message": `User ${username} not found :(`
         })
     }
-    if (! (await passwordIsValid(password, userAccount.password as string))){
+    if (!(await passwordIsValid(password, userAccount.password as string))) {
         return res.status(400).send({
-            "message":"Invalid password"
+            "message": "Invalid password"
         })
     }
     return res.status(200).send({
-        "message":"Login succcessfull",
-        "data":userAccount.id
+        "message": "Login succcessfull",
+        "data": userAccount.id
     })
 })
 
 
-app.listen(3000, async ()=>{
+app.listen(3000, async () => {
     await connectToDB()
     console.log('App listening on port 3000');
 })
