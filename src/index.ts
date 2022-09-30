94a432ad1384d823c29b2bb62d3b2685d5ea0edf
TheWisePigeon <zozozozeph@gmail.com>
2022-09-30 10:31:54 +0000

    arch: switched architecture

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, { Response, Request } from "express";
-import auth from './routes/auth'
 
 const app = express()
 
@@ -7,7 +6,6 @@ app.get('/', (req: Request, res: Response)=>{
     return res.send('Hello bruh ok')
 })
 
-app.use("/auth",auth)
 
 app.listen(3000, ()=>{
     console.log('App listening on port 3000');
--- src/routes/auth/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import authService from "../../services/Auth"
-const router = express.Router()
-
-router.route("/login").get(authService.login) 
-
-export default router 
